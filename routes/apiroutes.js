54e6f9a56cc5b27de2427c5b75dc323307dbfdb6
Ben Townsend <RBT000011@gmail.com>
2021-10-12 19:41:39 -0500

    updates to API routes and HTML routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,25 @@
 const fs = require("fs");
-const notes = require("./db/db.json")
+let notes = require("../db/db.json")
 const app = require("express").Router;
+
+app.get("/api/routes", function(req, res){
+    notes = fs.readFileSync("./db/db.json")
+    console.log("get", notes)
+    res.json(notes)
+})
+
+
+app.post("/api/routes", function(req, res){
+    let newNote = {
+        id: Math.floor(Math.random() * 1000 ),
+        title: req.body.title,
+        next: req.body.text
+    }
+    notes.push(newNote)
+    fs.writeFileSync("./db/db.json", notes, function(err, data) {
+        if (err) throw err;
+    })
+
+    console.log("post", notes)
+    res.json(notes)
+})
